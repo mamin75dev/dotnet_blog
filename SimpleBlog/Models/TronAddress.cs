862de189dd4a4de10713974c8872d8bf7042db14
mohamadamin <mamin75.dev@gmail.com>
2023-01-02 08:23:41 +0330

    tron address bug fix

--- SimpleBlog/Models/TronAddress.cs
+++ SimpleBlog/Models/TronAddress.cs
@@ -7,8 +7,8 @@ namespace SimpleBlog.Models;
 
 public class TronAddress
 {
-    
-    /*  Use the public key P as the input, by SHA3 get the result H.
+    /*
+        Use the public key P as the input, by SHA3 get the result H.
         The length of the public key is 64 bytes, SHA3 uses Keccak256. 
         Use the last 20 bytes of H, and add a byte of 0x41 in front of it, 
         then the address come out. Do basecheck to address, here is the final address. 
@@ -31,19 +31,19 @@ public class TronAddress
         decompressPublicKey = decompressPublicKey.Substring(2);
         var hash = Sha3.Sha3256().ComputeHash(decompressPublicKey.FromHexToByteArray());
         var numArray = new byte[20];
-        Array.Copy((Array) hash, hash.Length - 20, (Array) numArray, 0, 20);
+        Array.Copy((Array)hash, hash.Length - 20, (Array)numArray, 0, 20);
         var input = "41" + numArray.ToHexString();
         var byteArray1 = input.FromHexToByteArray();
         var sourceArray = Sha256.HashTwice(byteArray1);
         var bytes = new byte[4];
-        Array.Copy((Array) sourceArray, (Array) bytes, 4);
+        Array.Copy((Array)sourceArray, (Array)bytes, 4);
         var byteArray2 = (input + bytes.ToHexString()).FromHexToByteArray();
         Console.WriteLine(byteArray2.ToHexString());
-        Array.Copy((Array) byteArray1, (Array) byteArray2, byteArray1.Length);
+        Array.Copy((Array)byteArray1, (Array)byteArray2, byteArray1.Length);
         Console.WriteLine(byteArray2.ToHexString());
         return Base58.Encode(byteArray2);
     }
-    
+
     private static string DecompressPublicKey(string publicKey)
     {
         var pubKey = new PubKey(publicKey);
